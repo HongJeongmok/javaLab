c534d6c700542068960d9736213f5c3ecb991efa
Administrator <Administrator@424-05>
2021-03-29 18:47:18 +0900

    chap10 실습

---
+++ chap10Lab/src/sec02_runtime_exception/NullPointExceptionExample.java
@@ -0,0 +1,13 @@
+package sec02_runtime_exception;
+
+public class NullPointExceptionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String data = null;
+		// data = "ȫ�浿";
+		System.out.println(data.toString());  // java.lang.NullPointerException
+
+	}
+
+}
---
+++ chap10Lab/src/sec05_try_with_resources/TryWithResourceExample.java
@@ -0,0 +1,29 @@
+package sec05_try_with_resources;
+
+public class TryWithResourceExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* FileInputStream fis = null;
+		try {
+			fis = new FileInputStream("file.txt");
+			fis.read();  // file.txt을 읽습니다.
+			throw new Exception();  // 예외를 강제로 발생시킨다.
+		} catch(Exception e) {
+			System.out.println("예외 처리 코드가 발생되었습니다.");
+		} finally {
+			fis.close();
+		}
+		*/
+		
+		// 자동으로 리소스를 닫는다.
+		try (FileInputStream fis = new FileInputStream("file.txt")) {
+			fis.read();  // file.txt을 읽습니다.
+			throw new Exception();  // 예외를 강제로 발생시킨다.
+		} catch(Exception e) {
+			System.out.println("예외 처리 코드가 발생되었습니다.");
+		}
+
+	}
+
+}
