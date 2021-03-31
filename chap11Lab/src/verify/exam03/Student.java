db7b1f2f72327c8b57e98ab4af638e4de62935d5
HongJeongmok <ghdwjdahr@naver.com>
2021-03-31 15:18:23 +0900

    chap11 실습

---
+++ chap11Lab/src/verify/exam03/Student.java
@@ -0,0 +1,23 @@
+package verify.exam03;
+
+public class Student {
+	private String studentNum;
+    public Student(String studentNum) {
+              this.studentNum = studentNum;
+    } 
+    public String getStudentNum() {
+              return studentNum;  } 
+    // 작성 위치  
+	@Override
+	public int hashCode() {
+		return studentNum.hashCode();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		Student student = (Student) obj;  // 강제형 변환, Object -> Student
+		if (studentNum.equals(student.getStudentNum())) {
+			return true;
+		}
+		return false;
+	}
+}
