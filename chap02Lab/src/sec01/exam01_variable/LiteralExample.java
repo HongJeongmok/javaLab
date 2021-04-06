5b1c9092715d7e162e31010214829ece178f751c
HongJeongmok <ghdwjdahr@naver.com>
2021-04-06 10:46:09 +0900

    chap02 실습

---
+++ chap02Lab/src/sec01/exam01_variable/LiteralExample.java
@@ -0,0 +1,25 @@
+package sec01.exam01_variable;
+
+// 클래스
+public class LiteralExample {
+
+	// mail 메소드 : 어떤 기능을(실행문)을 수행한다.
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 변수 선언
+		// 자료형 변수 = 리터럴;
+		int var1 = 10;
+		
+		// 표준입출력(콘솔)에서 var1 변수를 출력한다.
+		System.out.println(var1);
+		
+		int var2 = 010; // 8진수
+		System.out.println(var2);
+		
+		int var3 = 0x10; //16진수 -> 16
+		System.out.println(var3);
+		
+	}
+
+}
